6818559d07813e05d96a2bb081440c60e3a54df5
Naisha <naisha.jain31@gmail.com>
2024-04-01 20:00:07 +0530

    upload1

--- app/src/main/java/com/example/carevista/SignUp.java
+++ app/src/main/java/com/example/carevista/SignUp.java
@@ -3,12 +3,18 @@ package com.example.carevista;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class SignUp extends AppCompatActivity {
 
+    TextInputEditText editTextName, editTextEmail, editTextPassword,  editText;
+    Button buttonSignup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        editTextEmail=findViewById()
     }
 }
